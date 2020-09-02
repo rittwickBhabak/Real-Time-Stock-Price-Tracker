566d165460351bd0deb464d23adef5cc7905a1f9
Rittwick <rittwick2019@gmail.com>
2020-09-03 00:07:38 +0530

    Company information is shifted to json file for better user experience

--- driver.py
+++ driver.py
@@ -1,18 +1,11 @@
 from datetime import datetime
 from scraper import scraper
+import json 
 
-list_of_urls = [
-    ['Google', 'https://www.google.com/search?q=tata+steel+stock'],
-    ['Alphabet', 'https://www.google.com/search?q=google+stock'],
-    ['Airtel', 'https://www.google.com/search?q=airtel+stock'],
-    ['Jindal Steel', 'https://www.google.com/search?q=jindal+steel+stock'],
-    ['HDFC Bank', 'https://www.google.com/search?q=hdfc+bank+stock+price'],
-    ['AXIS Bank', 'https://www.google.com/search?q=axis+bank+stock+price'],
-    ['SBIN', 'https://www.google.com/search?q=sbin+stock+price'],
-    ['TATA Motors', 'https://www.google.com/search?q=tata+motors+stock+price'],
-    ['Mahindra', 'https://www.google.com/search?q=mahindra+share+price'],
-    ['ZEEL', 'https://www.google.com/search?q=zeel+stock+price']
-]
+list_of_urls = []
+with open('company.json', 'r+') as file:
+    data = json.load(file)
+    list_of_urls = data['companies']
 
 print(f'Time {datetime.now()}')
 print('{:15} {:10}'.format('Company', 'Price'))
